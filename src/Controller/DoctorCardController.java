23b369d334c0e79cf86be9f9e5c8d5dfc518800b
2252Bao <22520118@gm.uit.edu.vn>
2025-04-29 15:49:56 +0700

    Cập nhật giao diện Doctor

--- src/Controller/DoctorCardController.java
+++ src/Controller/DoctorCardController.java
@@ -1,24 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import Model.DoctorData;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
+
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 /**
- *
- * @author WINDOWS 10
+ * Controller cho giao diện hiển thị thông tin bác sĩ.
  */
 public class DoctorCardController implements Initializable {
 
@@ -37,24 +35,36 @@ public class DoctorCardController implements Initializable {
     @FXML
     private Label doctor_email;
 
-    private Image image;
 
-//    public void setData(DoctorData dData) {
-//
-//        if (dData.getImage() != null) {
-//            image = new Image("File:" + dData.getImage(), 52, 52, false, true);
-//            doctor_circle.setFill(new ImagePattern(image));
-//        }
-//
-//        doctor_id.setText(dData.getDoctorID());
-//        doctor_fullName.setText(dData.getFullName());
-//        doctor_specialization.setText(dData.getSpecialized());
-//        doctor_email.setText(dData.getEmail());
-//    }
+
+    /**
+     * Thiết lập dữ liệu bác sĩ vào giao diện.
+     * @param dData Đối tượng DoctorData chứa thông tin bác sĩ.
+     */
+    public void setData(DoctorData dData) {
+       
+    }
+
+    @FXML
+    private void openDoctorPage() {
+        System.out.println("Đang mở DoctorPage...");
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/DoctorPage.fxml"));
+            Parent root = loader.load();
+
+            // Tạo cửa sổ mới để hiển thị thông tin bác sĩ
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Doctor Details");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        // Controller sẵn sàng để hiển thị dữ liệu
     }
-
 }
