63c94856b5c918979d7d36bbefbe5ed611827684
thanh thanh le <thanhthanh.lethi133@gmail.com>
2025-05-10 00:10:39 +0700

    Create clinic.sql, add fullmodel, add CRUD service for admin role, add lib folder

--- src/Model/ReceptionistData.java
+++ src/Model/ReceptionistData.java
@@ -1,38 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// ReceptionistFullData.java
 package Model;
 
-import java.security.Timestamp;
+public class ReceptionistFullData {
+    private String receptionistId, username, password, name, email, gender, phone, address;
+    private boolean isConfirmed;
 
-/**
- *
- * @author WINDOWS 10
- */
-public class ReceptionistData {
+    public ReceptionistFullData(String receptionistId, String username, String password, String name,
+                                 String email, String gender, String phone, String address, boolean isConfirmed) {
+        this.receptionistId = receptionistId;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.isConfirmed = isConfirmed;
+    }
 
-	private String receptionistId; // CHAR(36)
-	private String phone; // VARCHAR(50)
-	private String address; // TEXT
-	
-	public ReceptionistData(String receptionistId, String phone, String address) {
-		super();
-		this.receptionistId = receptionistId;
-		this.phone = phone;
-		this.address = address;
-	}
-	public String getReceptionistId() {
-		return receptionistId;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public String getAddress() {
-		return address;
-	}
-
-	
-
+    public String getReceptionistId() { return receptionistId; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getGender() { return gender; }
+    public String getPhone() { return phone; }
+    public String getAddress() { return address; }
+    public boolean isConfirmed() { return isConfirmed; }
 }
